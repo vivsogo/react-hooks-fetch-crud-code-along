5d2b450975949f099967e878d70fc5d8ff0fff02
Vivian Gomez <viviangomez0@gmail.com>
2022-03-19 13:45:35 -0400

    complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,7 +6,27 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
-
+   //here we are updating state.. our goal is to replace the empty items state with the new array from the server 
+   // we do that by using setitems inside the .then response 
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+  function handleUpdateItem(updatedItem){
+    const updatedItems= items.map((item)=>{
+      if (item.id === updatedItem.id){
+        return updatedItem;
+      } else{
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+  function handleAddItem(newItem){
+     setItems([...items,newItem]);
+    console.log("In ShoppingList:", newItem)
+  }
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
@@ -19,14 +39,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem}/>
         ))}
       </ul>
     </div>
